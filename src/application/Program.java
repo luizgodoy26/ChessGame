a5ab9099179d7e1043d7c3b3b192afadaa318547
Luiz <luizgodoy26@gmail.com>
2019-10-24 20:07:02 -0300

    Created classes Board and Piece / Modified Program to print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,19 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
     public static void main(String [] args){
 
-        
+        Board board = new Board(8, 8);
+
+        for (int i =0; i < board.getRows(); i++){
+            for (int j =0; j < board.getColumns(); j++){
+                System.out.print("X");
+            }
+            System.out.println(" ");
+        }
     }
 }
